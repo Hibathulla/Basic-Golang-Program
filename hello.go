d581180b5f8f4957fdf3c3ef4b3cfdfed05e1c5d
Hibathulla <57030901+Hibathulla@users.noreply.github.com>
2020-12-18 17:24:37 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-var id float32 = 5283
-var mark float32 = 100
+var name string = "Hibathulla"
+var id int = 8
+var age int = 20
+var mark float32 = 20
 
 func main() {
-	fmt.Println("My name is hibathulla and my id is", id, "and my mark is", mark)
+	fmt.Println("My name is", name, "and i am", age, "years old and my id is", id, "and my mark is", mark)
 }
